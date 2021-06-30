b065aa687e1a62992858d9045c376b45fc5d08b4
MaLIE2018 <liebsch@pm.me>
2021-06-30 19:34:10 +0200

    Update TrackRow.jsx

--- spotify/src/components/playlist/TrackRow.jsx
+++ spotify/src/components/playlist/TrackRow.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
 import { theme } from "../../index";
+import { addToQueue } from "../../redux/actions";
 import { TrackRowContainer, Track, AlbumTitle, TrackNumber } from "./styles";
-import { Link } from "react-router-dom";
+
 const TrackRow = (props) => {
   const dispatch = useDispatch();
   const player = useSelector((state) => state.player);
@@ -16,6 +17,7 @@ const TrackRow = (props) => {
         onClick={(e) => {
           e.preventDefault();
           dispatch({ type: "SET_TRACK", payload: track });
+          dispatch(addToQueue(track.trackListURL ?? track.albumId));
           dispatch({ type: "PLAY_TRACK" });
         }}
       >
